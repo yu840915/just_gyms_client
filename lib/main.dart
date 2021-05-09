78dba9394468a221c433f600a9e6965c2fa3054c
yu840915 <yu840915@gmail.com>
2021-05-10 00:14:37 +0800

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:where_gym/app_bloc.dart';
+import 'package:where_gym/error_view.dart';
 import 'package:where_gym/home_page.dart';
 import 'package:where_gym/intro/intro_page.dart';
 import 'package:where_gym/intro/permission_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
   await AppTrackingTransparency.requestTrackingAuthorization();
 }
@@ -18,12 +21,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<FirebaseApp>(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return MaterialApp(
+            home: ErrorPage(snapshot.error ?? '未知的錯誤，請重新開啟'),
+            debugShowCheckedModeBanner: false,
+          );
+        }
+        return _buildApp(context);
+      },
+    );
+  }
+
+  Widget _buildApp(BuildContext context) {
     return BlocProvider(
       create: (context) => AppBloc(null),
       child: MaterialApp(
-        title: 'JustGym',
+        title: 'Just Gyms',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
