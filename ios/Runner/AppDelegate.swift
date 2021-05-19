7c2b030ce2492ab50b9e1a96a086d217a899e2ed
yu840915 <yu840915@gmail.com>
2021-05-19 13:53:26 +0800

    Update for tracking

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import AppTrackingTransparency
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,11 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 14, *) {
+        ATTrackingManager.requestTrackingAuthorization { (status) in
+            print(status)
+        }    
+    }
     GMSServices.provideAPIKey("AIzaSyBXoHy5VAJODIpy3etPWpXA4cL1f9gNIVY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
