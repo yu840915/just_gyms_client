0964d6e72903d9ef2a190e1f9334c4b4c7203b2e
yu840915 <yu840915@gmail.com>
2021-05-23 21:57:17 +0800

    Replace mixpanel

--- lib/tracking/tracking.dart
+++ lib/tracking/tracking.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
-import 'package:native_mixpanel/native_mixpanel.dart';
+import 'package:mixpanel_flutter/mixpanel_flutter.dart';
 import 'package:where_gym/tracking/event_names.dart';
 
 class Tracker {
@@ -12,19 +9,13 @@ class Tracker {
   Tracker._(this.mixpanel);
 
   static Future<void> initialize() async {
-    Mixpanel mixpanel;
-    if (Platform.isIOS) {
-      mixpanel = Mixpanel(isOptedOut: false, shouldLogEvents: !kReleaseMode);
-    } else {
-      mixpanel = Mixpanel(isOptedOut: false, shouldLogEvents: !kReleaseMode);
-    }
-    await mixpanel.initialize('10e3acbe2c0245a34cdb4b232f35878f');
-    _instance = Tracker._(mixpanel);
+    _instance =
+        Tracker._(await Mixpanel.init('10e3acbe2c0245a34cdb4b232f35878f'));
   }
 
-  Future track(EventName event, [Map<String, dynamic> properties]) async {
+  void track(EventName event, [Map<String, dynamic> properties]) {
     try {
-      await mixpanel.track(event.name, properties ?? {});
+      mixpanel.track(event.name, properties: properties ?? {});
     } catch (e) {
       print('Failed to track event (${event.name}), error $e');
     }
