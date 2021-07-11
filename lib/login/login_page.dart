121335d3ac418dad537ed1a89950e5ebf05fb6c4
yu840915 <yu840915@gmail.com>
2021-07-11 14:41:59 +0800

    Auth icons

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:where_gym/gen/assets.gen.dart';
 import 'package:where_gym/login/authenticators.dart';
+import 'package:where_gym/shared_appearances.dart';
 
 class LoginPage extends StatelessWidget {
   void _performSignIn(Future<UserCredential> task) async {
@@ -15,19 +18,25 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Center(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Column(
-            children: [
-              _buildFBButton(context),
-              _buildGoogleButton(context),
-              _buildAppleButton(context),
-            ],
-            mainAxisSize: MainAxisSize.min,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              children: [
+                _buildAppleButton(context),
+                SizedBox(height: 12),
+                _buildFBButton(context),
+                SizedBox(height: 12),
+                _buildGoogleButton(context),
+              ],
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+            ),
           ),
         ),
       ),
@@ -35,26 +44,68 @@ class LoginPage extends StatelessWidget {
   }
 
   Widget _buildFBButton(BuildContext context) {
-    return TextButton(
-        onPressed: () {
-          _performSignIn(Authenticators.signInWithFacebook());
-        },
-        child: Text('Facebook'));
+    final color = const Color(0xff1877F2);
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        textStyle: TextStyles.large.action,
+        side: BorderSide(color: color),
+        primary: color,
+        minimumSize: Size(double.infinity, 40),
+      ),
+      onPressed: () {
+        _performSignIn(Authenticators.signInWithFacebook());
+      },
+      child: Row(
+        children: [
+          SvgPicture.asset(Assets.images.facebookLogo, width: 30, height: 30),
+          SizedBox(width: 24),
+          Text('使用 Facebook 登入')
+        ],
+      ),
+    );
   }
 
   Widget _buildGoogleButton(BuildContext context) {
-    return TextButton(
-        onPressed: () async {
-          _performSignIn(Authenticators.signInWithGoogle());
-        },
-        child: Text('Google'));
+    final color = Colors.black54;
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        textStyle: TextStyles.large.action,
+        side: BorderSide(color: color),
+        primary: color,
+        minimumSize: Size(double.infinity, 40),
+      ),
+      onPressed: () async {
+        _performSignIn(Authenticators.signInWithGoogle());
+      },
+      child: Row(
+        children: [
+          SvgPicture.asset(Assets.images.googleLogo, width: 30, height: 30),
+          SizedBox(width: 24),
+          Text('使用 Google 登入')
+        ],
+      ),
+    );
   }
 
   Widget _buildAppleButton(BuildContext context) {
-    return TextButton(
-        onPressed: () async {
-          _performSignIn(Authenticators.signInWithApple());
-        },
-        child: Text('Apple'));
+    final color = Colors.black;
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        textStyle: TextStyles.large.action,
+        side: BorderSide(color: color),
+        primary: color,
+        minimumSize: Size(double.infinity, 40),
+      ),
+      onPressed: () async {
+        _performSignIn(Authenticators.signInWithApple());
+      },
+      child: Row(
+        children: [
+          SvgPicture.asset(Assets.images.appleLogo, width: 30, height: 30),
+          SizedBox(width: 24),
+          Text('使用 Apple 登入')
+        ],
+      ),
+    );
   }
 }
