03467d95203a2be84d689eae1946617a5c721705
yu840915 <yu840915@gmail.com>
2021-07-20 00:36:23 +0800

    Init fcm

---
+++ lib/me/fcm_initialization.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:where_gym/app_bloc.dart';
+
+class FCMInitialization {
+  final AppBloc bloc;
+  FirebaseMessaging get _messaging => FirebaseMessaging.instance;
+  FCMInitialization(this.bloc);
+  bool _hasIntialized = false;
+
+  Future<void> initializeIfNeeded() async {
+    if (_hasIntialized) {
+      return;
+    }
+    await _requestPermission();
+    await _updateToken();
+    _hasIntialized = true;
+  }
+
+  Future<void> _requestPermission() async {
+    if (!Platform.isIOS) {
+      return;
+    }
+    await _messaging.requestPermission(provisional: true);
+  }
+
+  Future<void> _updateToken() async {
+    final token = await _messaging.getToken();
+    print(token);
+  }
+}
