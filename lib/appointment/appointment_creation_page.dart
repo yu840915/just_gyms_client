f976072334d8f92f9af70b11688da590366bea7e
yu840915 <yu840915@gmail.com>
2021-09-25 23:39:15 +0800

    Layout

--- lib/appointment/appointment_creation_page.dart
+++ lib/appointment/appointment_creation_page.dart
@@ -4,6 +4,7 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:where_gym/app_bar_factory.dart';
 import 'package:where_gym/appointment/appointment_time_composer.dart';
 import 'package:where_gym/gym.dart';
+import 'package:where_gym/shared_appearances.dart';
 
 class AppointmentCreatePage extends StatefulWidget {
   final Gym gym;
@@ -71,14 +72,26 @@ class _AppointmentCreatePageState extends State<AppointmentCreatePage> {
           locale: Intl.systemLocale,
           focusedDay: tomorrow,
           firstDay: tomorrow,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+          ),
+          calendarStyle: CalendarStyle(            
+            selectedDecoration: BoxDecoration(
+              color: AppColors.theme,
+              shape: BoxShape.circle,
+            ),
+          ),
+          rowHeight: 80,
           lastDay: tomorrow.add(Duration(days: 14)),
           calendarFormat: CalendarFormat.week,
           rangeSelectionMode: RangeSelectionMode.disabled,
           onDaySelected: (date, _) => _composer.setDay(date),
           selectedDayPredicate: (date) => _composer.isDaySelected(date),
+          eventLoader: null, //TODO: Add events
         ),
-        Spacer(),
-        SafeArea(
+        Container(
+          color: Colors.grey.shade200,
           child: StreamBuilder<TimeRange>(
             stream: _composer.onTimeRange,
             builder: (context, snapshot) {
@@ -86,31 +99,45 @@ class _AppointmentCreatePageState extends State<AppointmentCreatePage> {
             },
           ),
         ),
+        Spacer(),
       ],
     );
   }
 
   Widget _buildTimeButtons(TimeRange range) {
-    return Column(
+    return Row(
       children: [
         TextButton(
           onPressed: () {
             _showStartTimePicker(context);
           },
           child: Text(_formatTime(range?.start, "開始時間")),
+          style: TextButton.styleFrom(
+            textStyle: TextStyles.large.action,
+            primary: AppColors.theme,
+          ),
         ),
+        Icon(Icons.navigate_next),
         TextButton(
           onPressed: () {
             _showEndTimePicker(context);
           },
           child: Text(_formatTime(range?.end, "結束時間")),
-          style: TextButton.styleFrom(),
+          style: TextButton.styleFrom(
+            textStyle: TextStyles.large.action,
+            primary: AppColors.theme,
+          ),
         ),
       ],
+      mainAxisAlignment: MainAxisAlignment.center,
     );
   }
 
   String _formatTime(TimeOfDay time, String placeholder) {
-    return time != null ? time.stringValue : placeholder;
+    String result = placeholder;
+    if (time != null) {
+      result = '$result ${time.stringValue}';
+    }
+    return result;
   }
 }
