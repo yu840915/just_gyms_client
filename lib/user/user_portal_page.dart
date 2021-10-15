6a1312425cf86c5d92a963b9b603ed9e72b6a987
yu840915 <yu840915@gmail.com>
2021-10-16 00:22:42 +0800

    user page

--- lib/user/user_portal_page.dart
+++ lib/user/user_portal_page.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:where_gym/admin_gym_list_page.dart';
 import 'package:where_gym/alert_factory.dart';
 import 'package:where_gym/app_bar_factory.dart';
@@ -32,6 +34,12 @@ class UserPortalPage extends StatelessWidget {
         builder: (context) => AdminGymListPage(bloc.adminGymList),
       ),
     );
+  }
+
+  void _copyId(BuildContext context) async {
+    AppBloc bloc = BlocProvider.of(context);
+    await Clipboard.setData(ClipboardData(text: bloc.userRef.id));
+    await Fluttertoast.showToast(msg: '已複製使用者ID');
   }
 
   void _requestAccountDeletion(BuildContext context) async {}
@@ -87,17 +95,26 @@ class UserPortalPage extends StatelessWidget {
   }
 
   Widget _buildBody(BuildContext context, {@required bool shouldShowAdminUi}) {
+    AppBloc bloc = BlocProvider.of(context);
     return Column(
       children: [
         _Row(
+          title: '使用者ID： ${bloc.userRef.id}',
+          style: TextStyles.large.action,
+          action: () => _copyId(context),
+        ),
+        Divider(height: 1),
+        _Row(
           title: '預約',
           style: TextStyles.large.action,
+          hasDetail: true,
           action: () => _showMyAppointments(context),
         ),
         Divider(height: 1),
         _Row(
           title: '收藏',
           style: TextStyles.large.action,
+          hasDetail: true,
           action: () => _showFavorites(context),
         ),
         Divider(height: 1),
@@ -105,6 +122,7 @@ class UserPortalPage extends StatelessWidget {
           _Row(
             title: '場租管理',
             style: TextStyles.large.action,
+            hasDetail: true,
             action: () => _showAdminGymList(context),
           ),
           Divider(height: 1),
@@ -117,7 +135,7 @@ class UserPortalPage extends StatelessWidget {
         if (Platform.isIOS) ...[
           Divider(height: 1),
           _Row(
-            title: '要求刪除帳號',
+            title: '刪除帳號',
             style: TextStyles.small.action.copyWith(color: Colors.redAccent),
             action: () => _requestAccountDeletion(context),
           ),
@@ -131,14 +149,30 @@ class _Row extends StatelessWidget {
   final String title;
   final TextStyle style;
   final Function action;
-  _Row({@required this.title, this.style, @required this.action});
+  final bool hasDetail;
+  _Row(
+      {@required this.title,
+      this.style,
+      @required this.action,
+      this.hasDetail = false});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: action,
       child: Container(
-        child: Text(title, style: style),
+        child: Row(
+          children: [
+            Text(title, style: style),
+            Spacer(),
+            if (hasDetail)
+              Icon(
+                Icons.arrow_forward_ios_rounded,
+                size: 20,
+                color: Colors.grey,
+              ),
+          ],
+        ),
         alignment: Alignment.centerLeft,
         padding: EdgeInsets.symmetric(horizontal: 16),
         height: 60,
