6ec98b7c2081513f0a7bc088268df4968bc40dc2
yu840915 <yu840915@gmail.com>
2021-12-14 23:47:47 +0800

    Avatar button

--- lib/user/avatar_button.dart
+++ lib/user/avatar_button.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:where_gym/user/avatar_uri_factory.dart';
 
 class AvatarButton extends StatelessWidget {
   final void Function()? action;
-  AvatarButton({this.action, Key? key}) : super(key: key);
+  final String userId;
+  final double size;
+  AvatarButton({required this.userId, this.action, this.size = 80, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(onPressed: action, child: CircleAvatar());
+    return TextButton(
+      onPressed: action,
+      style: TextButton.styleFrom(
+        padding: EdgeInsets.zero,
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        shape: StadiumBorder(),
+      ),
+      child: Ink(
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(size / 2),
+          image: DecorationImage(
+            image: NetworkImage(
+              AvatarUriFactory.uriForUserId(userId).toString(),
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: null,
+      ),
+    );
   }
 }
--- lib/user/my_profile_pane.dart
+++ lib/user/my_profile_pane.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:where_gym/app_bloc.dart';
 import 'package:where_gym/user/avatar_button.dart';
 
 class MyProfilePane extends StatefulWidget {
@@ -10,13 +12,23 @@ class _MyProfilePaneState extends State<MyProfilePane> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(vertical: 16),
       color: Colors.transparent,
       height: 120,
       width: double.infinity,
+      child: Column(
+        children: [
+          _buildAvatarButton(context),
+        ],
+      ),
     );
   }
 
-  Widget _build(BuildContext context) {
-    return AvatarButton();
+  Widget _buildAvatarButton(BuildContext context) {
+    AppBloc bloc = BlocProvider.of(context);
+    return AvatarButton(
+      userId: bloc.userRef!.id,
+      action: () {},
+    );
   }
 }
